426c9be883bc9e6a8f0879d13a0a4f7cb3cca78f
Rose Wang <rose.wang@appian.com>
2016-08-24 18:17:19 -0400

    Starter Project

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = React.createClass({
+  render() {
+    return (
+      <div>
+        <h2>ChatMe</h2>
+      </div>
+    );
+  }
+});
+
+export default App;
