d2fa30fbdddec439076b0123f6d676220f098554
Rose Wang <rose.wang@appian.com>
2016-08-24 18:17:19 -0400

    Starter Project

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var config = {
+   entry: './main.js',
+
+   output: {
+      path:'./',
+      filename: 'index.js',
+   },
+
+   devServer: {
+      inline: true,
+      port: 8081
+   },
+
+   module: {
+      loaders: [
+         {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+
+            query: {
+               presets: ['es2015', 'react']
+            }
+         },
+         {
+           test: /\.less$/,
+           loader: "style!css!less"
+         }
+      ]
+   }
+}
+
+module.exports = config;
