2550095dcdac83855e6ad45ee543369776cf09a0
Rose Wang <rose.wang@appian.com>
2016-09-01 19:30:55 -0400

    Adding New Messages
    Create an input component below the list of messages. It is a set of
    divs and inputs within the App.jsx component.

--- App.jsx
+++ App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Firebase from './firebase-wrapper'; // Import Firebase library
 import Message from './components/Message.jsx';
+import Input from './components/Input.jsx';
 
 const App = React.createClass({
   getInitialState() {
     return {
-      messages: [] // Initialize empty list of messages
+      messages: [], // Initialize empty list of messages
+      name: 'Bob',
+      newMessage: ''
     };
   },
 
@@ -19,6 +22,29 @@ const App = React.createClass({
     Firebase.turnOff();
   },
 
+  handleNameChange(event) {
+    this.setState({name: event.target.value});
+  },
+
+  handleMessageChange(event) {
+    this.setState({newMessage: event.target.value});
+  },
+
+  handleKeyPress(event) {
+    const {name, newMessage} = this.state;
+    // If name or newMessage are blank, do not send new message
+    if (!name || !newMessage) {
+      return;
+    }
+
+    // If user hits Enter key, then send message to Firebase database
+    // and clear out the message box
+    if (event.key === 'Enter') {
+      Firebase.sendMessage({ name: name, message: newMessage });
+      this.setState({newMessage: ''});
+    }
+  },
+
   renderMessageDiv(message) {
     return <Message key={message.key} message={message}/>;
   },
@@ -28,11 +54,17 @@ const App = React.createClass({
     // for each message
     const messageDivs = this.state.messages.map(this.renderMessageDiv);
 
+    const {newMessage, name} = this.state;
+
     return (
       <div>
         <h2>ChatMe</h2>
         <div>
           {messageDivs}
+        </div>
+        <div>
+          <Input label={'Message'} value={newMessage} onChange={this.handleMessageChange} onKeyPress={this.handleKeyPress} />
+          <Input label={'Name'} value={name} onChange={this.handleNameChange} />
         </div>
       </div>
     );
--- components/Input.jsx
+++ components/Input.jsx
@@ -9,6 +9,21 @@ const Input = React.createClass({
   },
 
   render() {
+    const {label, value, onChange, onKeyPress} = this.props;
+    // We can now pass in event handlers to allow users to
+    // interact with this component
+    return (
+      <div>
+        <label htmlFor={label}>{label}</label>
+        <input
+          id={label}
+          type="text"
+          value={value}
+          onChange={onChange}
+          onKeyPress={onKeyPress}
+        />
+      </div>
+    );
   }
 });
 
