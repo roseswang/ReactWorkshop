81524d1ad47c2671342fbd9ae3c48698c935788e
Rose Wang <rose.wang@appian.com>
2016-10-26 11:55:30 -0400

    Adding Emoji Support

--- components/Message.jsx
+++ components/Message.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as emoji from 'node-emoji';
 
 const Message = React.createClass({
   // Setting propTypes ensure that your component is used correctly
@@ -13,7 +14,9 @@ const Message = React.createClass({
     // the message object
     const {name, message} = this.props.message;
 
-    return <p>{name}: {message}</p>;
+    const emojifiedString = emoji.emojify(message);
+
+    return <p>{name}: {emojifiedString}</p>;
   }
 });
 
